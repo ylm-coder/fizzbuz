25515183ba370d361fc771220b4026aad7ad8c11
anl-hub <56341033+anl-hub@users.noreply.github.com>
2021-04-10 11:34:43 +0300

    Add files via upload

---
+++ fizzbuz.rb
@@ -0,0 +1,17 @@
+class Fizzbuz
+    attr_reader :thing
+    def initialize(thing)
+        @thing = thing
+    end
+    # sayilar = [*1..100]
+    # sayilar.each do |sayi|
+    #     if sayi %15 == 0
+    #         puts "FizzBuz"
+    #     elsif sayi %3 == 0
+    #         puts "Fizz"
+    #     elsif sayi % 5 == 0
+    #         puts "Buzz"
+    #     else
+    #         puts sayi
+    #     end
+end
